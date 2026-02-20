8c23bc770d2132fe4bc4cc89f5bea07d8c4cb3ac
Paco <fgb39@gcloud.ua.es>
2026-02-20 10:31:32 +0100

    Crear clases Sala y Recurso

---
+++ sala.py
@@ -0,0 +1,7 @@
+class Sala:
+    def __init__(self, id, nombre, tipo, capaciadad, disponible):
+        self.id = id
+        self.nombre = nombre
+        self.tipo = tipo
+        self.capaciadad = capaciadad
+        self.disponible = disponible
