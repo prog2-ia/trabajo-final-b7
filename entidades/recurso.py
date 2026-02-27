a0e211516349a52d9232d071950bd49c3c882d02
Paco <fgb39@gcloud.ua.es>
2026-02-27 10:32:41 +0100

    Creación métodos clase Recurso

--- entidades/recurso.py
+++ entidades/recurso.py
@@ -4,3 +4,26 @@ class Recurso:
         self.nombre = nombre
         self.tipo = tipo
         self.disponible = disponible
+
+    def esta_disponible(self):
+        return self.disponible
+
+    def reservar(self):
+        if self.disponible:
+            return True
+        else:
+            return False
+
+    def liberar(self):
+        if not self.disponible:
+            return True
+
+    def es_tipo(self):
+        return self.tipo
+
+    def coincide_id(self, otro_id):
+        if otro_id == self.id:
+            return True
+
+    def es_valido(self):
+        return (self.id != "") and (self.nombre != "") and (self.tipo is not None)
