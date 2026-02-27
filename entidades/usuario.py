ece4f2df64fb84ec72eba16342ad8da8559106f4
Adri <apc229@alu.ua.es>
2026-02-27 10:16:51 +0100

    Redefinir clase usuario

--- entidades/usuario.py
+++ entidades/usuario.py
@@ -1,6 +1,9 @@
 class Usuario:
-    def __init__(self, dni, nombre, email, tipo):
+    def __init__(self, dni, username, nombre, apellidos, email, telefono, tipo):
         self.dni = dni
+        self.username = username
         self.nombre = nombre
+        self.apellidos = apellidos
         self.email = email
+        self.telefono = telefono
         self.tipo = tipo
